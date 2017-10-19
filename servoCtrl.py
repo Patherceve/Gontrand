0386e6eac356d1e66b10cb972b55fa8d9c6aaa8d
patherceve <pazra@uncc.edu>
2017-10-18 21:16:04 -0400

    Dual servo and servoRange[] on servoCtrl

--- servoCtrl.py
+++ servoCtrl.py
@@ -7,7 +7,7 @@ import time
 sCtrl = Adafruit_PCA9685.PCA9685()
 
 # Pulse lengths
-servo_range = [150, 200, 250, 300, 350, 400, 450, 500, 550, 600]
+servoRange = [150, 200, 250, 300, 350, 400, 450, 500, 550, 600]
 
 def setup():
     # Set frequency to 60hz, good for servos.
@@ -15,20 +15,24 @@ def setup():
 
 def loop():
     while True:
-        for r in range(150, 601, 50):
+        for r in servoRange:
             changePulse(r)
-        for r in range(600, 149, -50):
+        for r in reversed(servoRange):
             changePulse(r)
     
 def changePulse(p):
-    sCtrl.set_pwm(2, 0, p)
+    sCtrl.set_pwm(1, 0, p)
+    sCtrl.set_pwm(0, 0, p)
     time.sleep(0.05)
-    sCtrl.set_pwm(2, 0, 0)
+    sCtrl.set_pwm(1, 0, 0)
+    sCtrl.set_pwm(0, 0, 0)
 
 def destroy():
-    sCtrl.set_pwm(2, 0, 350)
-    time.sleep(1)
-    sCtrl.set_pwm(2, 0, 0)
+    sCtrl.set_pwm(1, 0, 350)
+    sCtrl.set_pwm(0, 0, 350)
+    time.sleep(0.5)
+    sCtrl.set_pwm(1, 0, 0)
+    sCtrl.set_pwm(0, 0, 0)
     
 if __name__ == '__main__':     
     setup()
