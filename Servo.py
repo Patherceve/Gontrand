85aabbfd693d6118c89f20add6e6eb258f9f1ee7
patherceve <pazra@uncc.edu>
2017-10-22 15:51:37 -0400

    change on servo pulse range 100-600, begin Servo class

---
+++ Servo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import Adafruit_PCA9685
+import time
+
+class Servo:
+    #Constructor
+    def __init__(self, channel, startPos):
+        self.pwm = Adafruit_PCA9685.PCA9685()
+        self.pulseRange = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600]
+        self.channel = channel
+        self.currentPos = startPos
+
+    #Instance method
+    def servoSetup(self):
+        pwm.set_pwm_freq(60)
+        pwm.set_pwm(self.channel, 0, self.pulseRange[currentPos])
+        time.sleep(0.3)
+        pwm.set_pwm(self.channel, 0, 0)
+    
+    def changePos(self, newPos):
+        if(self.currentPos > newPos)
+            #
+        elif(self.currentPos < newPos)
+            #
+        else
+            pwm.set_pwm(self.channel, 0, 0)
+        self.currentPos = newPos
--- servoCtrl.py
+++ servoCtrl.py
@@ -7,11 +7,13 @@ import time
 sCtrl = Adafruit_PCA9685.PCA9685()
 
 # Pulse lengths
-servoRange = [150, 200, 250, 300, 350, 400, 450, 500, 550, 600]
+servoRange = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600]
 
 def setup():
+    global channel
     # Set frequency to 60hz, good for servos.
     sCtrl.set_pwm_freq(60)
+    #channel = int(input('Enter channel: '))
 
 def loop():
     while True:
@@ -19,20 +21,21 @@ def loop():
             changePulse(r)
         for r in reversed(servoRange):
             changePulse(r)
+        #changePulse(int(input('Enter freq: ')))
     
 def changePulse(p):
-    sCtrl.set_pwm(1, 0, p)
     sCtrl.set_pwm(0, 0, p)
-    time.sleep(0.05)
-    sCtrl.set_pwm(1, 0, 0)
+    sCtrl.set_pwm(1, 0, p)
+    time.sleep(0.08)
     sCtrl.set_pwm(0, 0, 0)
+    sCtrl.set_pwm(1, 0, 0)
 
 def destroy():
-    sCtrl.set_pwm(1, 0, 350)
     sCtrl.set_pwm(0, 0, 350)
-    time.sleep(0.5)
-    sCtrl.set_pwm(1, 0, 0)
+    sCtrl.set_pwm(1, 0, 350)
+    time.sleep(0.3)
     sCtrl.set_pwm(0, 0, 0)
+    sCtrl.set_pwm(1, 0, 0)
     
 if __name__ == '__main__':     
     setup()
